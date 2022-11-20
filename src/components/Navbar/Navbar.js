2186b4182a37fd741d700801aebf12ad94352aea
Toasty201 <matdelva@gmail.com>
2022-11-20 15:34:51 -0300

    componente de Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css' 
+
+const Navbar = () => {
+    return(
+        <nav className='navbar'>
+            <h1 style={{color: 'blue'}}> Mi App</h1>
+            <button className='btn'> bton1</button>
+            <button className='btn'> bton2</button>
+        </nav>
+
+    )
+
+}
+export default Navbar 
